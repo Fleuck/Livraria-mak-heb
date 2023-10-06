68a24e34c330d4cfb6ec4b79e49c3ac42f454bf3
Felipe de Souza Leuck <felipe.leuck@pucpr.edu.br>
2023-10-05 21:24:53 -0300

    Criando repositório git com biblioteca e livro

---
+++ src/Biblioteca.java
@@ -0,0 +1,13 @@
+public abstract class Biblioteca extends Livro{
+    protected int Valor;
+
+    public Biblioteca(int codigo, String nome, String autor, String editora, int quantidade, int valor) {
+        super(codigo, nome, autor, editora, quantidade);
+        Valor = valor;
+    }
+
+    void cadastrar(int codigo){}
+    void remover(int codigo){}
+    void adicionar(int codigo){}
+    void consultar(int codigo){}
+}
